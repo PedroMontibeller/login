84f52737d60a7cf451418f997fc254746b56c625
Pedro Montibeller <pedromontt2003@hotmail.com>
2019-09-24 21:27:33 -0300

    Formulário de recuperação de senha
    close #2

--- index.php
+++ index.php
@@ -59,6 +59,32 @@
             </div>
         </section>
         <!-- Formulário de login ^ -->
+
+        <!-- Formulário de recuperação de senha v -->
+        <section class="row mt-2">
+            <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaLogin">
+                <h2 class="text-center mt-2">Gerar Nova Senha</h2>
+
+                <form action="#" method="post" id="formSenha" class="p-2">
+                    <div class="form-group">
+                        <small class="text-muted">Para gerar uma nova senha digite o seu email. Clique no link gerado.</small>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="email" name="emailGerarSenha" id="emailGerarSenha" class="form-control" placeholder="Email de recuperação de senha">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="submit" value="Gerar" name="btnGerar" id="btnGerar" class="btn btn-primary btn-block">
+                    </div>
+
+                    <div class="form-group">
+                        <p class="text-center">Já registrado? <a href="#" id="btnJaRegistrado">Entrar por aqui</a></p>
+                    </div>
+                </form>
+            </div>
+        </section>
+        <!-- Formulário de recuperação de senha ^ -->
     </main>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
