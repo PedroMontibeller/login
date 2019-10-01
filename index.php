a0dfb1577272faf30c3d5c79bc295a55ae6d1f56
Pedro Montibeller <pedromontt2003@hotmail.com>
2019-09-30 21:18:12 -0300

    Formulario de cadastro de novo usuario
    Close#3

--- index.php
+++ index.php
@@ -10,6 +10,12 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>Sistema de Login</title>
+
+    <style>
+        #alerta, #caixaSenha, #caixaRegistro{
+            display: none;
+        }
+    </style>
 </head>
 
 <body class="bg-dark">
@@ -53,7 +59,7 @@
                     </div>
 
                     <div class="form-group">
-                        <p clas="text-center">Novo usuário? <a href="#" id="btnRegistrar">Registre-se aqui</a></p>
+                        <p clas="text-center">Novo usuário? <a href="#" id="btnRegistrarNovo">Registre-se aqui</a></p>
                     </div>
                 </form>
             </div>
@@ -61,8 +67,8 @@
         <!-- Formulário de login ^ -->
 
         <!-- Formulário de recuperação de senha v -->
-        <section class="row mt-2">
-            <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaLogin">
+        <section class="row mt-5">
+            <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaSenha">
                 <h2 class="text-center mt-2">Gerar Nova Senha</h2>
 
                 <form action="#" method="post" id="formSenha" class="p-2">
@@ -103,6 +109,21 @@
                     <div class="form-group">
                         <input type="password" name="senha" id="senha" class="form-control" placeholder="Senha">
                     </div>
+                    <div class="form-group">
+                        <input type="password" name="confirmarSenha" id="confirmarSenha" class="form-control" placeholder="Confirmar senha">
+                    </div>
+                    <div class="form-group">
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" name="concordar" id="concordar" class="custom-control-input">
+                            <label for="concordar" class="custom-control-label">Eu concordo com os <a href="#">termos e condições.</a></label>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" value="Registrar" name="btnRegistrar" id="btnRegistrar" class="btn btn-primary btn-block">
+                    </div>
+                    <div class="form-group">
+                        <p class="text-center">Já registrado? <a href="#" id="btnJaRegistrado2">Entrar por aqui</a></p>
+                    </div>
                 </form>
             </div>
         </section>
@@ -114,6 +135,10 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script>
+        //Código jQuery para mostrar formulários v
+    </script>
 </body>
+
 
 </html>
