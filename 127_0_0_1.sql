aa9b523a6543283c73fa6df4fb6bdd61f1500ad2
Pedro Montibeller <pedromontt2003@hotmail.com>
2019-11-19 01:20:22 -0200

    Atualização do banco de dados
    Adicionado toke e tempoDeVida

--- 127_0_0_1.sql
+++ 127_0_0_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 07-Out-2019 às 21:26
+-- Tempo de geração: 19-Nov-2019 às 01:16
 -- Versão do servidor: 10.3.16-MariaDB
 -- versão do PHP: 7.3.7
 
@@ -38,8 +38,19 @@ CREATE TABLE `usuario` (
   `nomeUsuario` varchar(45) NOT NULL,
   `emailUsuario` varchar(45) NOT NULL,
   `senhaUsuario` char(40) NOT NULL,
-  `dataCriado` date NOT NULL
+  `dataCriado` date NOT NULL,
+  `urlImagemPerfil` varchar(100) NOT NULL,
+  `token` char(10) NOT NULL,
+  `tempoDeVida` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`idUsuario`, `nomeCompleto`, `nomeUsuario`, `emailUsuario`, `senhaUsuario`, `dataCriado`, `urlImagemPerfil`, `token`, `tempoDeVida`) VALUES
+(1, 'Pedrao', 'Pedrao', 'pedro@senai.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', '2019-11-19', '0', '', '2019-11-19 03:08:27'),
+(2, 'Pedraoo', 'Pedrao2', 'pedro2@senai.com', '20eabe5d64b0e216796e834f52d61fd0b70332fc', '2019-11-19', 'https://http2.mlstatic.com/pacoca-pacoquita-doce-de-amendoim-caixa-com-100-unidades-D_NQ_NP_723190-M', '', '2019-11-19 03:12:16');
 
 --
 -- Índices para tabelas despejadas
@@ -61,7 +72,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idUsuario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `idUsuario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
